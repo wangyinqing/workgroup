dd6c6d01cd841bba3b6a911ea76c5e6c549673c6
wangyinqing <wangyinqing1986@gmail.com>
2013-02-09 17:28:43 +0800

    updated sorted link list

--- wangyinqing's space/Practice/src/cn/edu/wyq/datastructure/link/Main.java
+++ wangyinqing's space/Practice/src/cn/edu/wyq/datastructure/link/Main.java
@@ -11,18 +11,37 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		String s = scan.next();
 		SortedLink<Integer> sl = new SortedLink<Integer>();
+		String s = scan.next();
 		while(!"q".equalsIgnoreCase(s)){
-			if(s.startsWith("d")){
-				int i = Integer.parseInt(s.substring(1,s.length()));
-				boolean deleted = sl.delete(i);
-				System.out.println(i + (deleted ? " was ": " was not ") + "deleted");
-			}else if(s.startsWith("p")){
+			if("size".equalsIgnoreCase(s)){
+				System.out.println("size is " + sl.size());
+			} else if("ra".equalsIgnoreCase(s)){
+				sl.removeAll();
+				System.out.println("array has been cleared");
+			} else if("d".equalsIgnoreCase(s)){
 				System.out.println(StringUtil.combine2String(sl.toArray(), "-->"));
-			}else{
-				int i = Integer.parseInt(s);
-				sl.insert(i);
+			} else if(s.startsWith("f")){
+				try{
+					Integer i = Integer.parseInt(s.substring(1,s.length()));
+					boolean found = sl.contains(i);
+					System.out.println(i + (found?" was ":" was not ") + "found");
+				}catch(NumberFormatException e){
+				}
+			} else if(s.startsWith("r")){
+				try{
+					Integer i = Integer.parseInt(s.substring(1,s.length()));
+					boolean removed = sl.delete(i);
+					System.out.println(i + (removed?" was ":" was not ") + "removed");
+				}catch(NumberFormatException e){
+				}
+			} else{
+				try{
+					Integer i = Integer.parseInt(s);
+					boolean added = sl.insert(i);
+					System.out.println(i + (added?" was ":" was not ") + "added");
+				}catch(NumberFormatException e){
+				}
 			}
 			s = scan.next();
 		}
--- wangyinqing's space/Practice/src/cn/edu/wyq/datastructure/link/SortedLink.java
+++ wangyinqing's space/Practice/src/cn/edu/wyq/datastructure/link/SortedLink.java
@@ -1,26 +1,63 @@
 package cn.edu.wyq.datastructure.link;
 
+/**
+ * Sorted linked list, all values inserted are sorting in ascending
+ * 
+ * @author wangyinqing
+ *
+ * @param <E>
+ */
 public class SortedLink<E extends Comparable<? super E>> {
 	private LinkedNode<E> first;
 	private int size = 0;
 	
 	public SortedLink(){
-		
 	}
 	
-	public void insert(E e){
+	/**
+	 * @param e
+	 * @return
+	 */
+	public boolean insert(E e){
+		boolean added = false;
 		if(first == null){
 			first = new LinkedNode<E>(e);
+			added = true;
 		}else{
 			LinkedNode<E> node = first;
-			while(node.next != null)
-				node = node.next;
-			node.next = new LinkedNode<E>(e);
+			LinkedNode<E> pre = null;
+			while(node != null){
+				if(node.value.compareTo(e) == 0){
+					return false;
+				}
+				if(node.value.compareTo(e) > 0){
+					if(pre == null){
+						first = new LinkedNode<E>(e);
+						first.next = node;
+					}else{
+						pre.next = new LinkedNode<E>(e);
+						pre.next.next = node;
+					}
+					added = true;
+					break;
+				}else{
+					pre = node;
+					node = node.next;
+				}
+			}
+			if(!added){
+				pre.next = new LinkedNode<E>(e);
+			}
 		}
 		size++;
+		return added;
 	}
 	
 	
+	/**
+	 * @param e
+	 * @return
+	 */
 	public boolean delete(E e) {
 		if (first == null) {
 			return false;
@@ -46,7 +83,7 @@ public class SortedLink<E extends Comparable<? super E>> {
 	public E[] toArray(){
 		E[] array = null; 
 		LinkedNode<E> node = first;
-		for(int i=0;i<size;i++){
+		for (int i = 0; i < size; i++) {
 			if(array == null)
 				array = (E[])java.lang.reflect.Array.newInstance(node.value.getClass(), size);
 			array[i] = node.value;
@@ -55,17 +92,45 @@ public class SortedLink<E extends Comparable<? super E>> {
 		return array;
 	}
 	
+	public int size(){
+		return size;
+	}
+	
+	
+	public void removeAll(){
+		LinkedNode<E> node = first;
+		while(node.next != null){
+			LinkedNode<E> temp = node.next;
+			node.next = null;
+			node = temp;
+			size--;
+		}
+		first = null;
+		size--;
+	}
+	
+	/**
+	 * @param e
+	 * @return
+	 */
+	public boolean contains(E e){
+		LinkedNode<E> node = first;
+		while(node != null){
+			if(node.value.compareTo(e) == 0)
+				return true;
+			node =node.next;
+		}
+		return false;
+	}
+	
 	static class LinkedNode<E>{
 		E value;
 		LinkedNode<E> next = null;
 		
-		
 		private LinkedNode(E e){
 			this.value = e;
 		}
-			
 	}
-	
 	
 	
 
