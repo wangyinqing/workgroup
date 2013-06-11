7843e3cc3c51c139cd17c1aada93f85c30f5969b
wangyinqing <wangyinqing1986@gmail.com>
2013-06-11 20:42:50 +0800

    initial version

---
+++ wangyinqing's space/my-mina/src/main/java/org/wyq/network/App.java
@@ -0,0 +1,13 @@
+package org.wyq.network;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ wangyinqing's space/my-mina/src/main/java/org/wyq/spring/Starter.java
@@ -0,0 +1,13 @@
+package org.wyq.spring;
+
+public class Starter implements IStater {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
